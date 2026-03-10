815157b2aa31d54bc04653e1e1fe06353b9d56b7
Gennaro Tortone <gennaro.tortone@na.infn.it>
2026-03-10 08:58:50 +0000

    MCCv1: change I2C addressing with mux kernel drivers

--- lib/MCCBoard.py
+++ lib/MCCBoard.py
@@ -3,7 +3,6 @@ import os
 import sys
 from functools import partial
 
-from lib.I2CSwitch import I2CSwitch
 from lib.POEController import POEController
 from lib.POESwitch import POESwitch
 from lib.SFP import SFP
@@ -77,25 +76,26 @@ class MCCBoard:
          I2C_BUS = 0
          I2C_SW0_ADDR = 0x71
          I2C_SW1_ADDR = 0x72
+
+         I2C_POE_BUS = 6
          I2C_POE0_ADDR = 0x20
          I2C_POE1_ADDR = 0x22
+
+         I2C_SFP0_BUS = 8
+         I2C_SFP1_BUS = 7
          I2C_SFP_ADDR = 0x50
 
-         isw1 = I2CSwitch(I2C_BUS, I2C_SW1_ADDR)
-         isw2 = I2CSwitch(I2C_BUS, I2C_SW0_ADDR)
-
          self.sw = POESwitch()
-         self.pc1 = POEController(I2C_BUS, I2C_POE0_ADDR, 
-            (partial(isw1.select, 3), partial(isw2.select, 0)))
-         self.pc2 = POEController(I2C_BUS, I2C_POE1_ADDR, 
-            (partial(isw1.select, 3), partial(isw2.select, 0)))
+         
+         self.pc1 = POEController(I2C_POE_BUS, I2C_POE0_ADDR, (),) 
+         self.pc2 = POEController(I2C_POE_BUS, I2C_POE1_ADDR, (),) 
 
          self.sw.add_controller(self.pc1)
          self.sw.add_controller(self.pc2)
 
          self.sfp = [
-            SFP(I2C_BUS, I2C_SFP_ADDR, (partial(isw1.select, 3), partial(isw2.select, 2))),
-            SFP(I2C_BUS, I2C_SFP_ADDR, (partial(isw1.select, 3), partial(isw2.select, 1)))
+            SFP(I2C_SFP0_BUS, I2C_SFP_ADDR),
+            SFP(I2C_SFP1_BUS, I2C_SFP_ADDR)
          ]
 
       elif self.version == 2:
